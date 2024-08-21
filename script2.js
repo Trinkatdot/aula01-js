16f911e70f9d7ab17a6d4fc72bd544f9825b2257
Trinkatdot <agatha.ktaureliano@senacsp.edu.br>
2024-08-20 22:14:21 -0300

    feat: atividade notas de 4 alunos

---
+++ script2.js
@@ -0,0 +1,8 @@
+//Faça um algoritmo onde o usuário informe o nome e 4 notas de dois alunos e mostre as médias desses 2 alunos.
+console.write("Calcule a sua nota")
+//informar o primeiro aluno
+var nome = window.prompt("informe o nome do primeiro aluno");
+//guarda a primira nota
+var nota1 = window.prompt("informe a primeira nota 1");
+//converte de texto para decimal
+nota1 = parseFloat(nota1);
